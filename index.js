63574a8354f291029764ab86bc4b69352b26bf25
Eduard Shavyrko <soc_35m@protonmail.com>
2019-01-21 22:35:27 +0200

    add static getChild(), _sink(), extractMax() methods

--- index.js
+++ index.js
@@ -2,20 +2,38 @@ class Heap {
     static getParent(index) {
         return index % 2 === 0 ? (index - 2) / 2 : (index - 1) / 2;
     }
+    static getChild(index) {
+        return {
+            left: index * 2 + 1,
+            right: index * 2 + 2
+        };
+    }
     constructor() {
         this.arr = [];
+        this.size = 0;
     }
-    _swap(child, parent) {
-        [this.arr[child], this.arr[parent]] = [this.arr[parent], this.arr[child]];
+    _swap(p, q) {
+        [this.arr[p], this.arr[q]] = [this.arr[q], this.arr[p]];
     }
-    _sink() {
-
+    _sink(index) {
+        while ((index + 1) * 2 <= this.size) { // last node with at least one leaf
+            const child = Heap.getChild(index);
+            let largest = child.left;
+            if (largest < this.size - 1 && this.arr[largest] < this.arr[child.right]) {
+                largest = child.right;
+            }
+            if (this.arr[largest] <= this.arr[index]) break;
+            this._swap(largest, index);
+            index = largest;
+        }
     }
     _swim(index) {
         while (index > 0) {
             let parentIndex = Heap.getParent(index);
             if (this.arr[index] > this.arr[parentIndex]) {
                 this._swap(index, parentIndex);
+            } else {
+                return;
             }
             index = parentIndex;
         }
@@ -26,9 +44,15 @@ class Heap {
     insert(p) {
         this.arr.push(p);
         this._swim(this.arr.length - 1);
+        this.size++;
+        return true;
     }
     extractMax() {
-
+        this._swap(0, this.arr.length - 1);
+        const max = this.arr.pop();
+        this.size--;
+        this._sink(0);
+        return max;
     }
     remove(p) {
 
@@ -47,6 +71,8 @@ h.insert(7);
 
 h.insert(2);
 h.insert(7);
-
-
-
+console.log(h.extractMax());
+console.log(h.extractMax());
+console.log(h.extractMax());
+console.log(h.extractMax());
+console.log(h.extractMax());
